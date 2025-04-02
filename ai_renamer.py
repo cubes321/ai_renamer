2e8c875e1aca3eb20c67ac9f9aaab5c075ed67b0
cubes321 <m.killingback@zen.co.uk>
2025-04-02 21:08:46 +0100

    First verion. Doesn't rename the file but produces the new name in the terminal.

--- ai_renamer.py
+++ ai_renamer.py
@@ -1,2 +1,32 @@
 # ai pdf renamer
-#
+# sys.argv[1] <- first command line argument
+import pathlib
+from google import genai
+from google.genai import types, errors
+import sys
+from google.genai.types import Tool, GenerateContentConfig, GoogleSearch
+
+with open('e:/ai/genai_api_key.txt') as file:
+    api_key = file.read().strip()
+client = genai.Client(api_key=api_key)
+
+sys_instruct="Answer in the form of a filename [company]_[date]_[invoice number]_[amount].pdf. The company name should be the first word in the text. The date should be in the format YYYY-MM-DD. The invoice number should be a unique identifier. The amount should be a number with two decimal places. If the text does not contain all of these elements, return 'Invalid input'."
+0
+filepath = pathlib.Path(sys.argv[1])
+
+
+try:
+    response = client.models.generate_content(
+        config=types.GenerateContentConfig(system_instruction=sys_instruct),
+        model = "gemini-2.0-flash",
+        contents=[
+            types.Part.from_bytes(
+              data=filepath.read_bytes(),
+              mime_type="application/pdf",
+            ),
+            "rename this pdf file to a new name"]
+    )
+    print(response.text)
+except genai.Error as e:
+    print(f"Error: {e.code} - {e.message}")
+    sys.exit(1)
